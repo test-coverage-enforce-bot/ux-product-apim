a546b113808ff7e325b117dc1edaa420860cad64
Imesh Chandrasiri <dimal.chandrasiri@gmail.com>
2016-03-28 18:07:16 +0530

    fixes for side panel resizing and animation

--- Markup/store/js/left-navigation.js
+++ Markup/store/js/left-navigation.js
@@ -10,41 +10,60 @@ $(window).load(function(){
             return;
         }
 
+        var delay = 0;
+        if($('.sublevel-menu').is(':visible')){
+            $('.sublevel-menu').slideUp(500);
+            delay = 700;
+        }
+
         var el = $(this),
             color = $(this).find('.left-menu-item').css('background-color'),
             prev = $(".navigation ul li:first"),
             distance = el.offset().top  - $(".navigation").offset().top,
-            isLastElClicked = el.next().length > 0 ? false : true;
+            isLastElClicked = el.next().length > 0 ? false : true,
+            hasSubMenu = el.find('.sublevel-menu').length == 1 ? true : false,
+            isActiveItem = el.hasClass('active');
 
+        if(isActiveItem && hasSubMenu){
+            if(el.find('.sublevel-menu').is(':visible')){
+                el.find('.sublevel-menu').slideUp();
+            }else{
+                el.find('.sublevel-menu').slideDown();
+            }
+            return;
+        }
 
+        setTimeout(function(){
+            distance = el.offset().top  - $(".navigation").offset().top;
+        },600);
 
         if (el.prev().length > 0) {
             animating = true;
             $(el).find('.left-menu-item').css('background-color',color);
             $.when(
-                el.animate({
+                el.delay(delay).animate({
                     top: -distance
                 }, 700)
             ).done(function () {
-                    el.insertBefore(prev).css('top','0px').addClass('active');
-                    el.find('.left-menu-item').removeAttr('style');
-                    if(isLastElClicked){
-                        prev.removeClass('active');
-                        
-                    }else{
-                        prev.removeClass('active');
-                    }
-                    animating = false;
+                el.insertBefore(prev).css('top','0px').addClass('active');
+                el.find('.left-menu-item').removeAttr('style');
+                if(isLastElClicked){
+                    prev.removeClass('active');
 
-                    if(el.find('.sublevel-menu').length == 1){
-                        el.find('.sublevel-menu').slideDown();
-                    }
+                }else{
+                    prev.removeClass('active');
+                }
+                animating = false;
 
-                    setTimeout(function(){
-                        //location.replace(el.attr('href'));
-                    },500)
+                if(hasSubMenu){
+                    el.find('.sublevel-menu').slideDown();
+                }
 
-                });
+                setTimeout(function(){
+                    //location.replace(el.attr('href'));
+                },500)
+
+            });
         }
     });
     

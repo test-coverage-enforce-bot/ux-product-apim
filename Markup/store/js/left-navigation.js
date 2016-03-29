e0cb1c86e3781f407b5014c919285eefae175e98
Imesh Chandrasiri <dimal.chandrasiri@gmail.com>
2016-03-29 11:01:08 +0530

    side menu implementation

--- Markup/store/js/left-navigation.js
+++ Markup/store/js/left-navigation.js
@@ -10,19 +10,21 @@ $(window).load(function(){
             return;
         }
 
-        var delay = 0;
-        if($('.sublevel-menu').is(':visible')){
-            $('.sublevel-menu').slideUp(500);
-            delay = 700;
-        }
-
-        var el = $(this),
+        var delay = 0,
+            el = $(this),
             color = $(this).find('.left-menu-item').css('background-color'),
             prev = $(".navigation ul li:first"),
             distance = el.offset().top  - $(".navigation").offset().top,
             isLastElClicked = el.next().length > 0 ? false : true,
             hasSubMenu = el.find('.sublevel-menu').length == 1 ? true : false,
             isActiveItem = el.hasClass('active');
+
+        if($('.sublevel-menu').is(':visible')){
+            $('.sublevel-menu').slideUp(function(){
+                console.log(el.offset().top  - $(".navigation").offset().top);
+            });
+            delay = 700;
+        }
 
         if(isActiveItem && hasSubMenu){
             if(el.find('.sublevel-menu').is(':visible')){
@@ -33,17 +35,18 @@ $(window).load(function(){
             return;
         }
 
-        setTimeout(function(){
-            distance = el.offset().top  - $(".navigation").offset().top;
-        },600);
-
         if (el.prev().length > 0) {
             animating = true;
             $(el).find('.left-menu-item').css('background-color',color);
             $.when(
                 el.delay(delay).animate({
                     top: -distance
-                }, 700)
+                },{
+                    duration:700,
+                    start:function(){
+                        console.log('Started');
+                    }
+                })
             ).done(function () {
                 el.insertBefore(prev).css('top','0px').addClass('active');
                 el.find('.left-menu-item').removeAttr('style');

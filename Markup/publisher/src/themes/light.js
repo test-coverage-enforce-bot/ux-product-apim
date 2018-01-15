9758781267dabf38c92e979c9cd672f9fccad740
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2018-01-15 12:35:39 +0530

    fix theme issues

--- Markup/publisher/src/themes/light.js
+++ Markup/publisher/src/themes/light.js
@@ -3,17 +3,12 @@ import purple from 'material-ui/colors/purple';
 import green from 'material-ui/colors/green';
 import red from 'material-ui/colors/red';
 
-
 const theme = createMuiTheme({
     palette: {
         type: 'light',
-        primary: green,
-        secondary: {
-            ...purple,
-        },
+        primary: { light: green[300], main: purple[500], dark: green[700] },
         error: red,
     },
 });
-
 
 export default theme;

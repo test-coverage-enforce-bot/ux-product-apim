13634623d30a0c3fea67c1afeb5bd079f1075194
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2018-01-17 13:04:14 +0530

    fix drawer overlap issue - override the Drawer

--- Markup/publisher/src/themes/light.js
+++ Markup/publisher/src/themes/light.js
@@ -18,15 +18,25 @@
 
 import { createMuiTheme } from 'material-ui/styles';
 import purple from 'material-ui/colors/purple';
+import blue from 'material-ui/colors/blue';
 import green from 'material-ui/colors/green';
 import red from 'material-ui/colors/red';
 
 const theme = createMuiTheme({
     palette: {
         type: 'light',
-        primary: { light: green[300], main: purple[500], dark: green[700] },
+        primary: { light: green[300], main: blue[800], dark: green[700] },
         error: red,
     },
+    overrides: {
+        MuiDrawer: {
+            paperAnchorLeft: {
+                top: 65,
+            },
+        }
+    }
+
+
 });
 
 export default theme;
